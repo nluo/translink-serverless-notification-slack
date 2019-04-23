8cbbc1195f898bfa00649c2a03adedd3bed1a0fb
Nick Luo <nick.luo@me.com>
2019-04-23 22:13:37 +1000

    Refactoring: replace request in favour of axios

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 "use strict";
 const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
-const request = require("request");
 const url = "https://gtfsrt.api.translink.com.au/Feed/SEQ";
+const axios = require("axios");
 
 const constructSlackText = (displayText, stopInfo) => {
   // return `${displayText}, upcoming is: ` + '```' + `${JSON.stringify(stopInfo, null, 4)}` + '```';
@@ -55,116 +55,102 @@ module.exports.webhook = async () => {
     };
   }
 
-  const requestSettings = {
-    method: "GET",
-    url,
-    encoding: null
-  };
-  return new Promise((resolve, reject) => {
-    return request(requestSettings, async (error, response, body) => {
-      if (error) {
-        return reject(JSON.stringify(error));
-      }
-      if (!error && response.statusCode == 200) {
-        try {
-          const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-          const now = new Date();
+  try {
+    const response = await axios({
+      method: "get",
+      url,
+      responseType: "arraybuffer"
+    });
 
-          const stopInfo = feed.entity
-            .map(function(entity) {
-              if (entity.trip_update) {
-                const { stop_time_update, trip } = entity.trip_update;
-       
-                if (trip.schedule_relationship == 3) {
-                  // console.log(entity);
-                  return null;
+    if (response.status == 200) {
+      const feed = GtfsRealtimeBindings.FeedMessage.decode(response.data);
+      const now = new Date();
+
+      const stopInfo = feed.entity
+        .map(function(entity) {
+          if (entity.trip_update) {
+            const { stop_time_update, trip } = entity.trip_update;
+
+            if (trip.schedule_relationship == 3) {
+              // console.log(entity);
+              return null;
+            }
+
+            if (stop_time_update.length) {
+              const stop = stop_time_update.find(s => s["stop_id"] == stop_id);
+
+              if (stop) {
+                console.log(JSON.stringify(stop));
+                let t;
+                if (stop.arrival) {
+                  t = new Date(stop.arrival.time.low * 1000);
+                } else {
+                  t = new Date(stop.arrival.time.low * 1000);
                 }
-                
-                if (stop_time_update.length) {
-                  const stop = stop_time_update.find(
-                    s => s["stop_id"] == stop_id
-                  );
 
-                  if (stop) {
-                    console.log(JSON.stringify(stop))
-                    let t;
-                    if (stop.arrival) {
-                      t = new Date(stop.arrival.time.low * 1000);
-                    } else {
-                      t = new Date(stop.arrival.time.low * 1000);
-                    }
-
-                    const info = {
-                      routeId: entity.trip_update.trip.route_id,
-                      arrivalTime: t
-                      // meta: entity
-                    };
-                    return info;
-                  }
-                }
+                const info = {
+                  routeId: entity.trip_update.trip.route_id,
+                  arrivalTime: t
+                  // meta: entity
+                };
+                return info;
               }
-            })
-            .filter(s => s)
-            .filter(s => {
-              console.log('route is ', route)
-            
-              if (!route || route === '') {
-                return true;
-              }
-              
-              const busRoute = s.routeId.split("-")[0];
-              
-              console.log('the bus route is ', busRoute);
-  
-              if (!busRoute) {
-                return true;
-              }
-  
-              return route == busRoute;
-  
-            })
-            .filter(s => {
-              return s.arrivalTime.getTime() - now.getTime() >= 0;
-            })
-            .sort((s1, s2) => s1.arrivalTime - s2.arrivalTime);
+            }
+          }
+        })
+        .filter(s => s)
+        .filter(s => {
+          console.log("route is ", route);
 
-          const localTimezoneStopInfo = stopInfo.map(s => {
-            return {
-              ...s,
-              arrivalTime: s.arrivalTime.toString()
-            };
-          });
-
-          console.log("after transformation, stop info is ", stopInfo);
-
-          if (stopInfo.length) {
-            const temp = stopInfo[0];
-            const busTimeTableText = calculateMinutesBetweenNow(
-              temp.arrivalTime
-            );
-            const busRoute = temp.routeId.split("-")[0];
-
-            const displayText = `The next Route:${busRoute} is coming to ${
-              stopName ? stopName : "stop"
-            } in ${busTimeTableText}`;
-
-            await postToSlack(
-              constructSlackText(displayText, localTimezoneStopInfo)
-            );
+          if (!route || route === "") {
+            return true;
           }
 
-          return resolve({
-            statusCode: 200,
-            body: JSON.stringify(localTimezoneStopInfo)
-          });
-        } catch (error) {
-          return reject({
-            statusCode: 500,
-            message: error.message,
-            stackTrace: error.stackTrace
-          })
-        }
+          const busRoute = s.routeId.split("-")[0];
+
+          console.log("the bus route is ", busRoute);
+
+          if (!busRoute) {
+            return true;
+          }
+
+          return route == busRoute;
+        })
+        .filter(s => {
+          return s.arrivalTime.getTime() - now.getTime() >= 0;
+        })
+        .sort((s1, s2) => s1.arrivalTime - s2.arrivalTime);
+
+      const localTimezoneStopInfo = stopInfo.map(s => {
+        return {
+          ...s,
+          arrivalTime: s.arrivalTime.toString()
+        };
+      });
+
+      console.log("after transformation, stop info is ", stopInfo);
+
+      if (stopInfo.length) {
+        const temp = stopInfo[0];
+        const busTimeTableText = calculateMinutesBetweenNow(temp.arrivalTime);
+        const busRoute = temp.routeId.split("-")[0];
+
+        const displayText = `The next Route:${busRoute} is coming to ${
+          stopName ? stopName : "stop"
+        } in ${busTimeTableText}`;
+
+        await postToSlack(
+          constructSlackText(displayText, localTimezoneStopInfo)
+        );
       }
-    });
-  });
+
+      return resolve({
+        statusCode: 200,
+        body: JSON.stringify(localTimezoneStopInfo)
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+
 };
