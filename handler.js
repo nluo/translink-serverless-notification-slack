f0baef0b016a140aaac7f86a30eb808487770ff7
Nick Luo <nick.luo@me.com>
2019-04-23 21:36:21 +1000

    Add logic to catch error

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const url = "https://gtfsrt.api.translink.com.au/Feed/SEQ";
 const constructSlackText = (displayText, stopInfo) => {
   // return `${displayText}, upcoming is: ` + '```' + `${JSON.stringify(stopInfo, null, 4)}` + '```';
   return displayText;
-}
+};
 
 const postToSlack = displayText => {
   const { slack_webhook } = process.env;
@@ -51,8 +51,8 @@ module.exports.webhook = async () => {
   if (!stop_id) {
     return {
       statusCode: 400,
-      message: 'You have to configure stop_id in serverless.yml'
-    }
+      message: "You have to configure stop_id in serverless.yml"
+    };
   }
 
   const requestSettings = {
@@ -66,77 +66,104 @@ module.exports.webhook = async () => {
         return reject(JSON.stringify(error));
       }
       if (!error && response.statusCode == 200) {
-        const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+        try {
+          const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+          const now = new Date();
 
-        const now = new Date();
-        const stopInfo = feed.entity
-          .map(function(entity) {
-            if (entity.trip_update) {
-              const { stop_time_update } = entity.trip_update;
-              if (stop_time_update.length) {
-                const stop = stop_time_update.find(
-                  s => s["stop_id"] === stop_id
-                );
+          const stopInfo = feed.entity
+            .map(function(entity) {
+              if (entity.trip_update) {
+                const { stop_time_update, trip } = entity.trip_update;
+       
+                if (trip.schedule_relationship == 3) {
+                  // console.log(entity);
+                  return null;
+                }
+                
+                if (stop_time_update.length) {
+                  const stop = stop_time_update.find(
+                    s => s["stop_id"] == stop_id
+                  );
 
-                if (stop) {
-                  const t = new Date(stop.arrival.time.low * 1000);
+                  if (stop) {
+                    console.log(JSON.stringify(stop))
+                    let t;
+                    if (stop.arrival) {
+                      t = new Date(stop.arrival.time.low * 1000);
+                    } else {
+                      t = new Date(stop.arrival.time.low * 1000);
+                    }
 
-                  const info = {
-                    routeId: entity.trip_update.trip.route_id,
-                    arrivalTime: t
-                    // meta: entity
-                  };
-                  return info;
+                    const info = {
+                      routeId: entity.trip_update.trip.route_id,
+                      arrivalTime: t
+                      // meta: entity
+                    };
+                    return info;
+                  }
                 }
               }
-            }
-          })
-          .filter(s => s)
-          .filter(s => {
-
-            return s.arrivalTime.getTime() - now.getTime() >= 0 
-          })
-          .filter(s => {
+            })
+            .filter(s => s)
+            .filter(s => {
+              console.log('route is ', route)
             
-            if (!route) {
-              return true;
-            }
-            
-            const busRoute = s.routeId.split("-")[0];
-            
-            console.log('the bus route is ', busRoute);
+              if (!route || route === '') {
+                return true;
+              }
+              
+              const busRoute = s.routeId.split("-")[0];
+              
+              console.log('the bus route is ', busRoute);
+  
+              if (!busRoute) {
+                return true;
+              }
+  
+              return route == busRoute;
+  
+            })
+            .filter(s => {
+              return s.arrivalTime.getTime() - now.getTime() >= 0;
+            })
+            .sort((s1, s2) => s1.arrivalTime - s2.arrivalTime);
 
-            if (!busRoute) {
-              return true;
-            }
+          const localTimezoneStopInfo = stopInfo.map(s => {
+            return {
+              ...s,
+              arrivalTime: s.arrivalTime.toString()
+            };
+          });
 
-            return route == busRoute;
+          console.log("after transformation, stop info is ", stopInfo);
 
-          })
-          .sort((s1, s2) => s1.arrivalTime - s2.arrivalTime)
+          if (stopInfo.length) {
+            const temp = stopInfo[0];
+            const busTimeTableText = calculateMinutesBetweenNow(
+              temp.arrivalTime
+            );
+            const busRoute = temp.routeId.split("-")[0];
 
-        const localTimezoneStopInfo = stopInfo.map(s => {
-          return {
-            ...s,
-            arrivalTime: s.arrivalTime.toString()
+            const displayText = `The next Route:${busRoute} is coming to ${
+              stopName ? stopName : "stop"
+            } in ${busTimeTableText}`;
+
+            await postToSlack(
+              constructSlackText(displayText, localTimezoneStopInfo)
+            );
           }
-        })
 
-        if (stopInfo.length) {
-          const temp = stopInfo[0];
-          const busTimeTableText = calculateMinutesBetweenNow(temp.arrivalTime);
-          const busRoute = temp.routeId.split("-")[0];
-
-          const displayText = `The next Route:${busRoute} is coming to ${stopName ? stopName : 'stop'} in ${busTimeTableText}`;
-
-          
-          await postToSlack(constructSlackText(displayText, localTimezoneStopInfo));
+          return resolve({
+            statusCode: 200,
+            body: JSON.stringify(localTimezoneStopInfo)
+          });
+        } catch (error) {
+          return reject({
+            statusCode: 500,
+            message: error.message,
+            stackTrace: error.stackTrace
+          })
         }
-
-        return resolve({
-          statusCode: 200,
-          body: JSON.stringify(localTimezoneStopInfo)
-        });
       }
     });
   });
